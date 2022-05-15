5873521a631995e13d7499e4f18c400e257e5e88
hgs-study <hgstudy_@naver.com>
2022-05-15 23:52:45 +0900

    ThreadLocal 동기화 - 적용

--- src/main/java/com/advanced/LogTraceConfig.java
+++ src/main/java/com/advanced/LogTraceConfig.java
@@ -2,14 +2,17 @@ package com.advanced;
 
 import com.advanced.trace.logtrace.FieldLogTrace;
 import com.advanced.trace.logtrace.LogTrace;
+import com.advanced.trace.logtrace.ThreadLocalLogTrace;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class LogTraceConfig {
 
+    // 구현체만 바꿈으로써 이점:
+    // 의존관계를 주입하는데에 클라이언트 코드를 바꾸지 않고 OCP를 지키면서 바꿀 수 있다
     @Bean
     public LogTrace logTrace(){
-        return new FieldLogTrace();
+        return new ThreadLocalLogTrace();
     }
 }
