7027ca123c4bafb8283dda95a15258108a1bc01f
hgs-study <hgstudy_@naver.com>
2022-05-19 01:23:58 +0900

    전략 패턴 - 예제2 (인수로 Strategy 받는 형식)

--- src/test/java/com/advanced/trace/strategy/ContextV1test.java
+++ src/test/java/com/advanced/trace/strategy/ContextV1test.java
@@ -1,6 +1,7 @@
 package com.advanced.trace.strategy;
 
 import com.advanced.trace.strategy.code.strategy.ContextV1;
+import com.advanced.trace.strategy.code.strategy.Strategy;
 import com.advanced.trace.strategy.code.strategy.StrategyLogic1;
 import com.advanced.trace.strategy.code.strategy.StrategyLogic2;
 import com.advanced.trace.template.code.AbstractTemplate;
@@ -50,6 +51,64 @@ public class ContextV1test {
         StrategyLogic2 strategyLogic2 = new StrategyLogic2();
         ContextV1 contextV2 = new ContextV1(strategyLogic2);
         contextV2.execute();
+    }
+
+    /**
+     * 전략 패턴 사용 (익명클래스)
+     */
+    @Test
+    void strategyV2(){
+        Strategy strategyLogic1 = new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        };
+        ContextV1 context1 = new ContextV1(strategyLogic1);
+        context1.execute();
+
+        Strategy strategyLogic2 = new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        };
+        ContextV1 context2 = new ContextV1(strategyLogic2);
+        context2.execute();
+    }
+
+    /**
+     * 전략 패턴 사용 (익명클래스)
+     */
+    @Test
+    void strategyV3(){
+        ContextV1 context1 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직1 실행");
+            }
+        });
+        context1.execute();
+
+        ContextV1 context2 = new ContextV1(new Strategy() {
+            @Override
+            public void call() {
+                log.info("비즈니스 로직2 실행");
+            }
+        });
+        context2.execute();
+    }
+
+    /**
+     * 전략 패턴 사용 (람다)
+     */
+    @Test
+    void strategyV4(){
+        ContextV1 context1 = new ContextV1(() -> log.info("비즈니스 로직1 실행"));
+        context1.execute();
+
+        ContextV1 context2 = new ContextV1(() -> log.info("비즈니스 로직2 실행"));
+        context2.execute();
     }
 
 }
